e21da60569da9de92b0989dac67992d22e4b724f
timothyshen <timothyshen1@hotmail.com>
2024-08-17 15:56:50 -0700

    feat: adding siwe

--- frontend/src/components/Header/WalletConnectHeader.tsx
+++ frontend/src/components/Header/WalletConnectHeader.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { AccountDisplayHeader } from "@/components/Header/AccountDisplayHeader";
 import WalletOptions from "@/components/WalletOption";
-import { useAccount } from "wagmi";
+import { useAccount, useSignMessage } from "wagmi";
+import { getTargetNetwork } from "@/utils/network";
 import {
     DialogTitle,
     Dialog,
@@ -11,34 +12,59 @@ import {
 } from "@/components/ui/dialog"
 
 import { Button } from "@/components/ui/button"
-import { useAutoConnect } from "@/hooks/useAutoConnect"
+import { SiweMessage } from "siwe"
+import { getCsrfToken, signIn } from "next-auth/react"
+import { useState } from "react";
+
 
 
 const WalletConnectComponentHeader = () => {
-    useAutoConnect()
-    const { isConnected } = useAccount()
+    const { signMessageAsync } = useSignMessage()
+    const currentChain = getTargetNetwork();
+
+    const [hasSigned, setHasSigned] = useState(false)
+
+    const { address, isConnected } = useAccount()
+
+    const handleSign = async () => {
+        if (!isConnected) open();
+        try {
+            const message = new SiweMessage({
+                domain: window.location.host,
+                uri: window.location.origin,
+                version: "1",
+                address: address,
+                statement: process.env.NEXT_PUBLIC_SIGNIN_MESSAGE,
+                nonce: await getCsrfToken(),
+                chainId: currentChain.id,
+            });
+
+            const signedMessage = await signMessageAsync({
+                message: message.prepareMessage(),
+            });
+
+            setHasSigned(true);
+
+            const response = await signIn("web3", {
+                message: JSON.stringify(message),
+                signedMessage,
+                redirect: true,
+                callbackUrl: '/hidden'
+            });
+            if (response?.error) {
+                console.log("Error occured:", response.error);
+            }
+
+        } catch (error) {
+            console.log("Error Occured", error);
+        }
+    };
 
     return (
         <div className="max-h-52 flex flex-col">
-            {isConnected ?
-                (<>
-                    <AccountDisplayHeader />
-                </>) :
-                (<>
-                    <Dialog>
-                        <DialogTrigger asChild>
-                            <Button variant="link" className='text-xl text-black'>
-                                Connect
-                            </Button>
-                        </DialogTrigger>
-                        <DialogContent>
-                            <DialogHeader>
-                                <DialogTitle>Are you absolutely sure?</DialogTitle>
-                            </DialogHeader>
-                            <WalletOptions />
-                        </DialogContent>
-                    </Dialog>
-                </>)}
+            {!isConnected && <Button onClick={() => handleSign()}>Sign In</Button>}
+            {isConnected && !hasSigned && <Button onClick={() => handleSign()}>Sign In</Button>}
+            {isConnected && hasSigned && <AccountDisplayHeader />}
         </div>
     )
 }
