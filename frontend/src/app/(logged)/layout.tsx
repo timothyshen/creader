1c719617f8528e66cfd8966c10ba47e3c64f1ca2
timothyshen <timothyshen1@hotmail.com>
2024-08-18 15:22:27 -0700

    refactor: layout for general page update

--- frontend/src/app/(logged)/layout.tsx
+++ frontend/src/app/(logged)/layout.tsx
@@ -1,9 +1,15 @@
 // app/author/layout.tsx
+'use client'
 import React from 'react';
 import { Header } from '@/components/Header/Header';
 import { Footer } from '@/components/Footer';
-import { Sidebar } from '@/components/Dashboard/SideBar';
 import { Inter } from 'next/font/google';
+import WalletOptions from "@/components/WalletOption";
+import { ConnectWalletClient } from "@/provider/viemConfig";
+import { getTargetNetwork } from "@/utils/network";
+
+import { useAccount } from "wagmi";
+
 import "../globals.css"; // Assuming you want to keep global styles consistent
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,12 +19,26 @@ interface AuthorLayoutProps {
 }
 
 export default function AuthorLayout({ children }: AuthorLayoutProps) {
+    const { isConnected } = useAccount()
+    const currentChain = getTargetNetwork();
+
+    if (isConnected) {
+        console.log('currentChain', currentChain);
+        const walletClient = ConnectWalletClient();
+        walletClient.switchChain(currentChain);
+    }
+
     return (
         <html lang="en">
             <body className={`${inter.className} flex min-h-screen w-full flex-col bg-muted/40`}>
                 <Header />
                 <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-                    {children}
+                    {isConnected ? children : (
+                        <>
+                            <div className="text-2xl font-bold mb-4 mx-auto">Connect to a wallet</div>
+                            <WalletOptions />
+                        </>
+                    )}
                 </main>
                 <Footer />
             </body>
